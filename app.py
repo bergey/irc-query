560d1572af0bf1a6c25630e219e3c0e6b79569ad
Daniel Bergey <bergey@alum.mit.edu>
2015-02-04 21:17:08 +0000

    JSON serialization, REST query endpoints

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from tables import *
+
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello You!'
+
+@app.route('/json')
+def json_demo():
+    return json.dumps(['Hello', 'World'])
+
+# setup DB session
+# TODO worry about threads & scope
+Base = declarative_base()
+engine = create_engine('postgresql://bergey:password@localhost')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/v1/presence')
+def all_presences():
+    return json.dumps(session.query(Presence).all(), cls=SqlJson)
+
+@app.route('/v1/channel/<c>')
+def channel_presence(c):
+    return json.dumps([p.user_name for p in session.query(Presence).filter(Presence.channel == c).all()], cls=SqlJson)
+
+@app.route('/v1/user/<u>')
+def user_presence(u):
+    return json.dumps([p.channel for p in session.query(Presence).filter(Presence.user_name == u).all()], cls= SqlJson)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- tables.py
+++ tables.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+import json
+
 Base = declarative_base()
 
 class Presence(Base):
@@ -11,6 +13,15 @@ class Presence(Base):
     channel = Column(Text, nullable=False)
     user_name = Column(Text, nullable=False)
 
+    def _json_enc(self):
+        return { 'channel': self.channel, 'user_name': self.user_name }
+
 engine = create_engine('postgresql://bergey:password@localhost')
 
 Base.metadata.create_all(engine)
+
+class SqlJson(json.JSONEncoder):
+    def default(self, o):
+        if hasattr(o, '_json_enc') and callable(o._json_enc):
+            return o._json_enc()
+        return json.JSONEncoder.default(self, o)
